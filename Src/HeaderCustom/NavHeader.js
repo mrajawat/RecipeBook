91ca3e05e2633a0c1c53ef3a6795de300eb8735d
mrajawat <msrajawat@gmail.com>
2022-11-17 10:56:47 +0530

    changes in header

--- Src/HeaderCustom/NavHeader.js
+++ Src/HeaderCustom/NavHeader.js
@@ -3,8 +3,18 @@ import React from 'react'
 
 const NavHeader = () => {
   return (
-    <View style={{ flex: .1, backgroundColor: "#eee", justifyContent: 'center', paddingHorizontal: 20 }}>
-      <Text style={{ fontSize: 20, fontWeight: 'bold', color: "black" }}>Recipie umhuh!</Text>
+    <View style={{
+      justifyContent: 'center',
+      padding: 10, flexDirection: 'row',
+      height: height - 690,
+      backgroundColor: "#eee",
+      justifyContent: 'center',
+      alignItems: 'center'
+    }}>
+      <View style={{}}><Octicons name='arrow-left' size={25} color={"black"} /></View>
+      <View style={{ flex: 1, alignItems: 'center' }}>
+        <Text style={{ fontSize: 20, fontWeight: 'bold', color: "black" }}>Recipie umhuh!</Text></View>
+      <View style={{}}><AntDesign name='bars' size={25} color={"black"} onPress={DetailsScreen} /></View>
     </View>
   )
 }
